39cb4002feea21a5b6cb6adc4d72f485c9e57441
VernonVega <mld.sphere@gmail.com>
2026-01-13 02:23:19 +0300

    Update run.sh

--- wyoming_stt_api/run.sh
+++ wyoming_stt_api/run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/with-contenv bashio
 
-export OPENAI_API_KEY=$(bashio::config 'openai_api_key')
+export ATS_URL=$(bashio::config 'ats_url')
 export MAX_AUDIO_DURATION_S=$(bashio::config 'max_audio_duration_s')
 
 python -m wyoming_stt_api
